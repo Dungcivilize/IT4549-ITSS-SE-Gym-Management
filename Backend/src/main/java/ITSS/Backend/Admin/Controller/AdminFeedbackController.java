c0bb91c7ae6f2bf61fc1c92b6093ae0e0a915490
Nguyễn Kim Ngọc <177725608+TumRoyall@users.noreply.github.com>
2025-06-07 08:59:15 +0700

    .

--- Backend/src/main/java/ITSS/Backend/Admin/Controller/AdminFeedbackController.java
+++ Backend/src/main/java/ITSS/Backend/Admin/Controller/AdminFeedbackController.java
@@ -31,5 +31,5 @@ public class AdminFeedbackController {
     public List<FeedbackDTO> getHighlightFeedbacks() {
         return feedbackService.getHighlightFeedbacks();
     }
-    // hello
+    // hello world
 }
